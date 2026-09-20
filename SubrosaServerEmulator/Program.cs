4623b93ac7e250e7f4deb0177a95a01a8ddceccd
N4T4NM <natanaelmarian@hotmail.com>
2026-09-20 15:10:35 -0300

    Avoid 100% cpu usage

--- SubrosaServerEmulator/Program.cs
+++ SubrosaServerEmulator/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using SubrosaServerEmulator;
 using SubrosaServerEmulator.Master;
 using SubrosaServerEmulator.Web;
+// ReSharper disable AccessToDisposedClosure
 
 var settings = ServerSettings.LoadFrom("config.json");
 
 LogSource.MinimumLogLevel = settings.LogLevel;
 
-var master = new MasterServer();
-var webserver = new WebServer(settings);
+using var master = new MasterServer();
+using var webserver = new WebServer(settings);
+
+using var shutdown = new ManualResetEventSlim(false);
+
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    shutdown.Set();
+};
+
+using var sigterm = PosixSignalRegistration.Create(PosixSignal.SIGTERM, ctx =>
+{
+    ctx.Cancel = true;
+    shutdown.Set();
+});
 
 try
 {
     master.Run(settings);
     webserver.Run();
+
+    shutdown.Wait();
 }
 catch (Exception ex)
 {
     Debug.WriteLine(ex);
     master.Error(ex.Message);
+    Environment.ExitCode = 1;
 }
-
-while (true);
